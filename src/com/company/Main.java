4690b08a94f6005790eed1d6081f025019250160
sushant singh <iit2018171@iiita.ac.in>
2022-02-21 19:01:45 +0530

    branch-4 changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Lets learn jdbc");
         System.out.println("Its a fun");
+        System.out.println("Will also do some praticle");
         java.sql.Connection con = null;
         Statement st = null;
 
