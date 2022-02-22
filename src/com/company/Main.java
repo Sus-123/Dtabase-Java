34c574bbe3e3a12281bf7f40697b65a15be037ed
sushant singh <iit2018171@iiita.ac.in>
2022-02-22 12:01:15 +0530

    branch-4 changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,37 +2,63 @@ package com.company;
 import com.sun.jdi.connect.spi.Connection;
 import java.sql.Statement;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
+
     static final String Driver = "com.mysql.jdbc";
     static final String url  = "jdbc:mysql://localhost/REGISTRATION";
     static final String user = "root";
     static final String pass = "Susu_1234";
 
     public static void main(String[] args) {
-        System.out.println("Lets learn jdbc");
-        System.out.println("Its a fun");
-        System.out.println("Will also do some praticle");
+
         java.sql.Connection con = null;
         Statement st = null;
-
+        String query = "select * from Student";
+       // String query2 = "select last from Student where id = 2 ";
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Connecting to selected database...");
             con = DriverManager.getConnection(url, user, pass);
             System.out.println("Connected to db successfully..");
-            System.out.println("Creating table in selected database..");
             st = con.createStatement();
 
-            String sql = "CREATE TABLE  REGISTRATION" +
-                    "(id INTEGER not null," +
-                    "first VARCHAR(255)," +
-                    "last VARCHAR(255)," +
-                    "age INTEGER, " +
-                    "primary key(id))";
-            st.executeUpdate(sql);
-            System.out.println("table created in given database");
+            //read
+            ResultSet resultSet = st.executeQuery(query);
+            String userData = "";
+            while (resultSet.next()) {
+                userData = resultSet.getInt(1) + " " + resultSet.getString(2) +" " + resultSet.getString(3);
+                //System.out.println(userData);
+            }
 
+            //-----------------------------------------------------------
+            // Insert values : for inserting we will use prepared statments.
+            // each question mark represents the individual coloumn.
+            String queryInsert = "insert into Student values (?,?,?,?)";
+            ArrayList<Integer> ids = new ArrayList<>();
+            ArrayList<String> first = new ArrayList<>();
+            ArrayList<String> last = new ArrayList<>();
+            ArrayList<Integer> age = new ArrayList<>();
+
+            for (int i = 0; i <3 ; i++) {
+                ids.add(i+10);
+                first.add("first" + i);
+                last.add("last" + i);
+                age.add(20+i);
+            }
+
+            PreparedStatement statement = con.prepareStatement(queryInsert);
+
+            for (int i = 0; i <3 ; i++) {
+               // int idx = ids.get(i);
+                statement.setInt(1, ids.get(i));
+                statement.setString(2,first.get(i));
+                statement.setString(3, last.get(i));
+                statement.setInt(4,age.get(i));
+                int rowsAffected = statement.executeUpdate();
+                System.out.println(rowsAffected);
+            }
 
         } catch (SQLException e){
             e.printStackTrace();
@@ -47,7 +73,6 @@ public class Main {
                 e.printStackTrace();
             }
         }
-
         try {
             if( con != null)
                 con.close();
@@ -56,7 +81,6 @@ public class Main {
         }
 
         System.out.println("Good bye!");
-
     }
 
 }
