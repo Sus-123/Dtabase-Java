9c03484d3a907e96b0635aab2a24d7efb591b02f
sushant singh <iit2018171@iiita.ac.in>
2022-03-03 13:37:36 +0530

    good-practice DOA

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,25 +9,17 @@ public class Main {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        PlayerService playerService = new PlayerService();
-        playerService.makeConnection();
 
         String name = "T3P3";
         Player player = new Player();
         player.name = name;
         player.run=100;
         player.balls= 100;
-        System.out.println("demo");
+        System.out.println("HELLO");
+
+        System.out.println("Ignored");
 
 
-        System.out.println("dcd");
-        System.out.println("ko");
-       // playerService.addPlayer(player);
-       //int id =  playerService.addInning("suresh6", 101);
-
-        int  run  = playerService.getPlayer("Pak");
-       // System.out.println(p1.name + " " + p1.run + " " + p1.balls);
-         System.out.println(run);
     }
 
 
